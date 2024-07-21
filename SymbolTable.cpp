93d649b6fec24c147b15af335fb96477e6e48110
marybayyouk <marybayyouk@campus.technion.ac.il>
2024-07-21 10:35:18 +0300

    added Symbol and SymbolTable classes

---
+++ SymbolTable.cpp
@@ -0,0 +1,46 @@
+#include "SymbolTeble.h"
+
+SymbolTable::SymbolTable(int maxOff, string retType = "") : symbols() { 
+    maxOffset = maxOff;
+    returnedType = new string(retType);
+}
+
+SymbolTable::~SymbolTable()
+{
+    delete returnedType;
+    for (Symbol* symbol : symbols) 
+         delete symbol; 
+}
+
+bool SymbolTable::isDefined(const string& name)
+{
+    for (const Symbol* symbol : symbols) {
+        if (symbol->getName() == name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Symbol* SymbolTable::findSymbol(const string& name)
+{   
+    if(!isDefined(name)) {
+        return nullptr;
+    }
+    for (Symbol* symbol : symbols) {
+        if (symbol->getName() == name) {
+            return symbol;
+        }
+    }
+}
+
+void SymbolTable::addSymbol(const Symbol& symbol) 
+{
+    if(!isDefined(symbol.getName())) {
+        return;
+    }
+    Symbol* newSymbol = new Symbol(symbol);
+    symbols.push_back(newSymbol);
+}
+
+
---
+++ SymbolTeble.h
@@ -0,0 +1,47 @@
+#ifndef __SYMBOLTABLE_H_
+#define __SYMBOLTABLE_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+struct NameTypeInfo {
+    string type;
+    vector<string> names;
+};
+
+class Symbol {
+    string symName;
+    int symOffset;
+    bool isFunc;
+    NameTypeInfo NameType;
+public:
+    Symbol(const string& name, int offset, bool isfunc, const string& type, vector<string> names) : symName(name), 
+                                                                        symOffset(offset) , isFunc(isfunc), NameType{type, names} {}
+    ~Symbol() = default;
+    ///GETTERS
+    string getName() const { return symName; }
+    int getOffset() const { return symOffset; }
+    bool getIsFunction() const { return isFunc; }
+    string getType() const { return NameType.type; }
+};
+
+class SymbolTable {
+    vector<Symbol*> symbols;
+    int maxOffset;
+    string* returnedType;
+public:
+    SymbolTable(int maxOff, string retType = "");
+    ~SymbolTable();
+
+    bool isDefined(const string& name);
+    Symbol* findSymbol(const string& symName);
+    void addSymbol(const Symbol& symbol);
+};
+
+
+
+#endif // __SYMBOLTABLE_H_
